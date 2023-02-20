c997e7cc97808d91767b62807f568950f21cdc40
Josh Manders <josh@joshmanders.com>
2023-02-20 11:27:57 -0600

    fix extend

--- src/react.ts
+++ src/react.ts
@@ -1,7 +1,7 @@
 import { Linter } from 'eslint';
 
 const config: Linter.Config = {
-  extends: ['./dist/base.js', 'plugin:react/recommended'],
+  extends: ['./index.js', 'plugin:react/recommended'],
   plugins: ['jsx-a11y', 'react-hooks'],
   parserOptions: {
     ecmaFeatures: {
